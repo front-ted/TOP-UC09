1e0ff2fb613582566c15daf7fe365512e2164801
ymbitancourt <115833606+ymbitancourt@users.noreply.github.com>
2024-04-12 19:59:15 -0300

    continuação

--- conteudo/js/game-custom.js
+++ conteudo/js/game-custom.js
@@ -1,5 +1,3 @@
-
-
 // Aqui você adiciona ou remove os containers pra onde devem ir os cards
 var containers = [
   // Container com os cards que serão realocados
@@ -19,6 +17,30 @@ var containers = [
   document.querySelector('#slot-10'),
   document.querySelector('#slot-11')
 ];
+
+var containers6 = [
+  // Container com os cards que serão realocados
+  // Não mexer
+  document.querySelector("#cardPilej2"),
+
+  // Containers que irão receber os cards
+  document.querySelector("#slot-5"),
+  document.querySelector('#slot-6'),
+  document.querySelector('#slot-7'),
+  document.querySelector('#slot-8'),
+  document.querySelector('#slot-9')
+];
+
+var containers8 = [
+  // Container com os cards que serão realocados
+  // Não mexer
+  document.querySelector("#cardPilej3"),
+
+  // Containers que irão receber os cards
+  document.querySelector("#slot-10"),
+  document.querySelector('#slot-11')
+];
+
 var audio = new Audio();
 var erro = 0;
 
@@ -65,4 +87,57 @@ dragula({
   audio.play(); //play
 });
 
-// document.addEventListener('touchmove', function(e) { e.preventDefault(); }, { passive:false });
+dragula({
+  containers: containers6,
+  revertOnSpill: true,
+  direction: 'vertical',
+  accepts: function (el, target, source, sibling) {
+    return el.dataset.target == target.id;
+  }
+}).on('drag', function (el, source) {
+  // On mobile this prevents the default page scrolling while dragging an item.
+  scrollable = false;
+}).on("drop", function () {
+  scrollable = true;
+
+  $('#bgmodal-acerto1').modal('show')
+  audio.setAttribute('src', 'audios/acerto.mp3'); //change the source
+  audio.load(); //load the new source
+  audio.play(); //play
+
+}).on("cancel", function () {
+  scrollable = true;
+
+  $('#bgmodal-erro1').modal('show')
+  audio.setAttribute('src', 'audios/erro.mp3'); //change the source
+  audio.load(); //load the new source
+  audio.play(); //play
+});
+
+dragula({
+  containers: containers8,
+  revertOnSpill: true,
+  direction: 'vertical',
+  accepts: function (el, target, source, sibling) {
+    return el.dataset.target == target.id;
+  }
+}).on('drag', function (el, source) {
+  // On mobile this prevents the default page scrolling while dragging an item.
+  scrollable = false;
+}).on("drop", function () {
+  scrollable = true;
+
+  $('#bgmodal-acerto2').modal('show')
+  audio.setAttribute('src', 'audios/acerto.mp3'); //change the source
+  audio.load(); //load the new source
+  audio.play(); //play
+
+}).on("cancel", function () {
+  scrollable = true;
+
+  $('#bgmodal-erro2').modal('show')
+  audio.setAttribute('src', 'audios/erro.mp3'); //change the source
+  audio.load(); //load the new source
+  audio.play(); //play
+});
+
